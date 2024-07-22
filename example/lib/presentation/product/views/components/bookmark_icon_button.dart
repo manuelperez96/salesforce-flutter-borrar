b922b1f7130a1b7ce23a46f19586ed322a868bdb
Patricia Manzanero <patricia.manzanero-jimenez@capgemini.com>
2024-07-22 12:40:42 +0200

    alertDialog

--- example/lib/presentation/product/views/components/bookmark_icon_button.dart
+++ example/lib/presentation/product/views/components/bookmark_icon_button.dart
@@ -2,15 +2,12 @@ import 'package:example/models/product_model.dart';
 import 'package:example/presentation/bookmark/bloc/bookmark_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class BookmarkIconButton extends StatelessWidget {
-  const BookmarkIconButton({
-    super.key,
-    required this.product,
-  });
-
   final ProductModel product;
+
+  const BookmarkIconButton({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +16,33 @@ class BookmarkIconButton extends StatelessWidget {
         if (state is BookmarkLoadedState) {
           if (state.products.contains(product)) {
             return IconButton(
-              onPressed: () => BlocProvider.of<BookmarkBloc>(context)
-                  .add(RemoveProductEvent(product)),
+              onPressed: () => showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('Eliminar producto'),
+                    content: const Text(
+                        '¿Seguro que quieres eliminar este producto?'),
+                    actions: <Widget>[
+                      TextButton(
+                        child: const Text('Cancelar'),
+                        onPressed: () {
+                          Navigator.of(context).pop(); // Cierra el diálogo
+                        },
+                      ),
+                      TextButton(
+                        child: const Text('Eliminar'),
+                        onPressed: () {
+                          // Elimina el producto y cierra el diálogo
+                          BlocProvider.of<BookmarkBloc>(context)
+                              .add(RemoveProductEvent(product));
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              ),
               icon: SvgPicture.asset("assets/icons/Bookmark.svg",
                   colorFilter: const ColorFilter.mode(
                     Colors.redAccent,
