3a9cc8563b2e0ba888f88245acd70a7117b9b7ca
Patricia Manzanero <patricia.manzanero-jimenez@capgemini.com>
2024-08-01 12:59:35 +0200

    test access token

---
+++ test/models/responses/access_token/access_token_test.dart
@@ -0,0 +1,52 @@
+import 'package:sf_commerce_sdk/models/responses/access_token/access_token.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group(
+    'AccessToken',
+    () {
+      group(
+        'Creation',
+        () {
+          test(
+            'When call constructor return instance of AccessToken',
+            () {
+              expect(
+                AccessToken(
+                  tokenType: 'Bearer',
+                  accessToken: 'access',
+                  expiresIn: 3600,
+                  refreshToken: 'asdf',
+                  refreshTokenExpiresIn: 30,
+                  usid: 'usid',
+                  customerId: 'customer_id',
+                  encUserId: 'enc_user_id',
+                ),
+                isNotNull,
+              );
+            },
+          );
+
+          test(
+            'When call fromJson return instance of AccessToken',
+            () {
+              expect(
+                AccessToken.fromJson({
+                  'token_type': 'Bearer',
+                  'access_token': 'access',
+                  'expires_in': 3600,
+                  'refresh_token': 'asdf',
+                  'refresh_token_expires_in': 30,
+                  'usid': 'usid',
+                  'customer_id': 'customer_id',
+                  'enc_user_id': 'enc_user_id',
+                }),
+                isNotNull,
+              );
+            },
+          );
+        },
+      );
+    },
+  );
+}
