d18b8622f21439fcbd5f4c68e2c866192d6519e0
carlos.guillen-miguel@capgemini.com <carlos.guillen-miguel@capgemini.com>
2024-08-02 10:38:50 +0200

    test auth_repository

--- test/repository/auth/auth_repository_test.dart
+++ test/repository/auth/auth_repository_test.dart
@@ -19,11 +19,21 @@ void main() {
   late MockDio dio;
   late MockTokenStorage storage;
   late MockInterceptors interceptors;
+  late SfCommerceConfig config;
+  late AuthRepository authRepo;
 
   setUp(() {
     dio = MockDio();
     storage = MockTokenStorage();
     interceptors = MockInterceptors();
+
+    config = SfCommerceConfig(
+      clientId: 'clientId',
+      organizationId: 'organizationId',
+      siteId: 'siteId',
+      host: 'https://host.com',
+    );
+    authRepo = AuthRepository(dio: dio, config: config, storage: storage);
 
     when(dio.interceptors).thenReturn(interceptors);
     when(interceptors.add(any)).thenReturn(null);
@@ -38,18 +48,43 @@ void main() {
           expect(
             AuthRepository(
               dio: dio,
-              config: SfCommerceConfig(
-                clientId: 'clientId',
-                organizationId: 'organizationId',
-                siteId: 'siteId',
-                host: 'https://host.com',
-              ),
+              config: config,
               storage: storage,
             ),
             isNotNull,
           );
         },
       );
+
+      // group(
+      //   'anonymousLogin()',
+      //   () {
+      //     test(
+      //       'When accessToken Endpoint failed, anonymous login UnableDoAnonymousLoginException',
+      //       () async {
+      //         when(
+      //           dio.post(
+      //             '${config.host}/shopper/auth/v1/organizations/${config.organizationId}/oauth2/token',
+      //             options: Options(
+      //               headers: {
+      //                 'Content-Type': 'application/x-www-form-urlencoded'
+      //               },
+      //             ),
+      //             data: anyNamed('data'),
+      //           ),
+      //         ).thenThrow(
+      //           Exception(),
+      //         );
+
+      //         try {
+      //           await authRepo.anonymousLogin();
+      //         } catch (e) {
+      //           expect(e, isA<UnableDoAnonymousLoginException>);
+      //         }
+      //       },
+      //     );
+      //   },
+      // );
     },
   );
 }
