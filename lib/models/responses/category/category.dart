85fb7d4870dcb258be0fc0df42bc5a87758ed06a
carlos.guillen-miguel@capgemini.com <carlos.guillen-miguel@capgemini.com>
2024-08-19 13:49:30 +0200

    add categories

---
+++ lib/models/responses/category/category.dart
@@ -0,0 +1,58 @@
+class Category {
+  String id;
+  String name;
+  int onlineSubCategoriesCount;
+  String? pageDescription;
+  String? pageTitle;
+  String parentCategoryId;
+  String? cHeaderMenuBanner;
+
+  Category({
+    required this.id,
+    required this.name,
+    required this.onlineSubCategoriesCount,
+    this.pageDescription,
+    this.pageTitle,
+    required this.parentCategoryId,
+    this.cHeaderMenuBanner,
+  });
+
+  factory Category.fromJson(Map<String, dynamic> json) {
+    print(json['id']);
+    print(
+      json['name'],
+    );
+    print(
+      json['onlineSubCategoriesCount'],
+    );
+    print(
+      json['pageDescription'],
+    );
+    print(
+      json['pageTitle'],
+    );
+    print(
+      json['parentCategoryId'],
+    );
+    print(json['c_headerMenuBanner'] != null);
+
+    return Category(
+      id: json['id'],
+      name: json['name'],
+      onlineSubCategoriesCount: json['onlineSubCategoriesCount'],
+      pageDescription: json['pageDescription'],
+      pageTitle: json['pageTitle'],
+      parentCategoryId: json['parentCategoryId'],
+      cHeaderMenuBanner: json['c_headerMenuBanner'] != null
+          ? parseBannerImage(json['c_headerMenuBanner'])
+          : '',
+    );
+  }
+
+  static String parseBannerImage(String original) {
+    String imageUrl = original.substring(original.indexOf('src'));
+    imageUrl = imageUrl.substring(imageUrl.indexOf('"') + 1);
+    imageUrl = imageUrl.substring(0, imageUrl.indexOf('"'));
+    return imageUrl;
+  }
+}
