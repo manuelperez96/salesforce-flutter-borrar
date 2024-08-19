99ee96036e10491f5129a89e3970fdf4b08b5712
carlos.guillen-miguel@capgemini.com <carlos.guillen-miguel@capgemini.com>
2024-08-19 16:11:36 +0200

    categories testing

--- lib/repository/category_repository.dart
+++ lib/repository/category_repository.dart
@@ -4,42 +4,18 @@ import 'package:sf_commerce_sdk/repository/repository.dart';
 
 class CategoryRepository extends Repository {
   late final String _pathRoot;
-  late CategoryWrapper rootWrapper;
 
   CategoryRepository({required super.dio, required super.config}) {
     _pathRoot =
         '${config.host}/product/shopper-products/v1/organizations/${config.organizationId}/categories?ids=root&siteId=${config.siteId}';
-    rootWrapper = CategoryWrapper(
-        category: Category(
-            id: 'root',
-            name: 'root',
-            onlineSubCategoriesCount: 0,
-            parentCategoryId: ''));
   }
 
-  void loadCategories() async {
-    List<Category> firstChildren = await getCategoriesByFather(_pathRoot);
-    rootWrapper.children = List.empty(growable: true);
-    for (var element in firstChildren) {
-      rootWrapper.children.add(await buildCategoryWrapper(element));
-    }
+  Future<List<Category>> getRootCategories() async {
+    return getCategoriesByUrl(_pathRoot);
   }
 
-  Future<CategoryWrapper> buildCategoryWrapper(Category category) async {
-    CategoryWrapper result = CategoryWrapper(category: category, children: []);
-    if (category.onlineSubCategoriesCount > 0) {
-      List<Category> categoryChildren = await getCategoriesByFather(
-          '${config.host}/product/shopper-products/v1/organizations/${config.organizationId}/categories?ids=${category.id}&siteId=${config.siteId}');
-      for (var element in categoryChildren) {
-        result.children.add(await buildCategoryWrapper(element));
-      }
-    }
-    return result;
-  }
-
-  Future<List<Category>> getCategoriesByFather(String url) async {
+  Future<List<Category>> getCategoriesByUrl(String url) async {
     try {
-      print(url);
       final response = await dio.get(url,
           options: Options(
             headers: {'Content-Type': 'application/json'},
@@ -53,11 +29,4 @@ class CategoryRepository extends Repository {
       throw Exception('Failed to load categories: $e');
     }
   }
-}
-
-class CategoryWrapper {
-  Category category;
-  List<CategoryWrapper> children;
-
-  CategoryWrapper({required this.category, this.children = const []});
 }
---
+++ test/repository/category_repository_test.dart
@@ -0,0 +1,93 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:sf_commerce_sdk/models/sf_commerce_config.dart';
+import 'package:sf_commerce_sdk/repository/category_repository.dart';
+
+import 'category_repository_test.mocks.dart';
+
+@GenerateNiceMocks(
+  [
+    MockSpec<Dio>(),
+    MockSpec<Response>(),
+  ],
+)
+void main() {
+  late MockDio mockDio;
+  late SfCommerceConfig config;
+  late CategoryRepository categoryRepository;
+  late MockResponse response;
+
+  setUp(() {
+    mockDio = MockDio();
+    response = MockResponse();
+
+    config = SfCommerceConfig(
+      clientId: 'clientId',
+      organizationId: 'organizationId',
+      siteId: 'siteId',
+      host: 'https://host.com',
+    );
+
+    categoryRepository = CategoryRepository(dio: mockDio, config: config);
+  });
+
+  group('CategoryRepository', () {
+    test(
+      'creation',
+      () async {
+        expect(
+          CategoryRepository(
+            dio: mockDio,
+            config: config,
+          ),
+          isNotNull,
+        );
+      },
+    );
+
+    group(
+      'getRootCategories()',
+      () {
+        test(
+          'When request fail, throw a Exception',
+          () async {
+            when(mockDio.get(any,
+                options: Options(
+                  headers: {'Content-Type': 'application/json'},
+                ))).thenThrow(Exception());
+
+            expect(() async => await categoryRepository.getRootCategories(),
+                throwsA(isA<Exception>()));
+          },
+        );
+      },
+    );
+
+    // group(
+    //   'success',
+    //   () {
+    //     test(
+    //       'When request success, return a List of Category',
+    //       () async {
+    //         when(mockDio.get(
+    //             'https://host.com/product/shopper-products/v1/organizations/organizationId/categories?ids=root&siteId=siteId',
+    //             options: Options(
+    //               headers: {'Content-Type': 'application/json'},
+    //             ))).thenAnswer(
+    //           (realInvocation) => Future.value(response),
+    //         );
+
+    //         when(response.data).thenReturn(responseJSON);
+
+    //         final List<Category> result =
+    //             await categoryRepository.getRootCategories();
+
+    //         expect(result, isNotEmpty);
+    //       },
+    //     );
+    //   },
+    // );
+  });
+}
