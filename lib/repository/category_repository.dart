7f564deaadc38d6ae9075a6774e022bba96c3ce2
carlos.guillen-miguel@capgemini.com <carlos.guillen-miguel@capgemini.com>
2024-08-26 13:22:57 +0200

    add local memory cache to category repository

--- lib/repository/category_repository.dart
+++ lib/repository/category_repository.dart
@@ -5,12 +5,11 @@ import 'package:sf_commerce_sdk/repository/repository.dart';
 
 class CategoryRepository extends Repository {
   late final String _pathRoot;
-  final MemoryCache memoryCache;
+  final MemoryCache<Category> memoryCache = MemoryCache<Category>();
 
-  CategoryRepository(
-      {required super.dio, required super.config, required this.memoryCache}) {
+  CategoryRepository({required super.dio, required super.config}) {
     _pathRoot =
-        '${config.host}/product/shopper-products/v1/organizations/${config.organizationId}/categories?ids=root&siteId=${config.siteId}';
+        '${config.host}/product/shopper-products/v1/organizations/${config.organizationId}/categories?ids=root&siteId=${config.siteId}&levels=2';
   }
 
   Future<List<Category>> getRootCategories() async {
@@ -19,26 +18,31 @@ class CategoryRepository extends Repository {
 
   Future<List<Category>> getCategoriesByUrl(String url) async {
     try {
-      //check is data is in cache
-      if (memoryCache.categoriesByUrl.containsKey(url)) {
-        return memoryCache.categoriesByUrl[url]!;
+      //check if data is in cache
+      if (memoryCache.hasListKey(url)) {
+        return memoryCache.getListValue(url);
       }
 
       final response = await dio.get(url,
           options: Options(
             headers: {'Content-Type': 'application/json'},
           ));
+
       final List<dynamic> jsonResponse = response.data['data'][0]['categories'];
 
       List<Category> result = jsonResponse
           .map((categoryJson) => Category.fromJson(categoryJson))
           .toList();
 
-      memoryCache.categoriesByUrl[url] = result;
+      memoryCache.addListValue(url, result);
 
       return result;
     } catch (e) {
       throw Exception('Failed to load categories: $e');
     }
+  }
+
+  void clearCache() {
+    memoryCache.clearAll();
   }
 }
