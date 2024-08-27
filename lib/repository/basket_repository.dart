53d3fabb9c73651366089b87d986b302d27d8504
Manuel Pérez Soto <manuelperezsoto@SELNK1350100586.corp.capgemini.com>
2024-08-27 13:02:08 +0200

    refactor: clean basket_repository warnings

--- lib/repository/basket_repository.dart
+++ lib/repository/basket_repository.dart
@@ -8,7 +8,7 @@ class BasketRepository extends Repository {
 
   Future<Basket> createBasket() async {
     try {
-      final response = await dio.post(
+      final response = await dio.post<dynamic>(
         '${config.host}/checkout/shopper-baskets/v1/organizations/${config.organizationId}/baskets?siteId=${config.siteId}',
         options: Options(
           headers: {'Content-Type': 'application/json'},
@@ -16,7 +16,7 @@ class BasketRepository extends Repository {
       );
 
       final dynamic jsonResponse = response.data;
-      Basket basket = Basket.fromJson(jsonResponse);
+      final basket = Basket.fromJson(jsonResponse as Map<String, dynamic>);
       _basketId = basket.basketId;
       return basket;
     } catch (e) {
@@ -27,7 +27,7 @@ class BasketRepository extends Repository {
   Future<Basket> getBasket() async {
     if (_basketId == null) await createBasket();
     try {
-      final response = await dio.get(
+      final response = await dio.get<dynamic>(
         '${config.host}/checkout/shopper-baskets/v1/organizations/${config.organizationId}/baskets/$_basketId?siteId=${config.siteId}',
         options: Options(
           headers: {'Content-Type': 'application/json'},
@@ -35,7 +35,7 @@ class BasketRepository extends Repository {
       );
 
       final dynamic jsonResponse = response.data;
-      Basket basket = Basket.fromJson(jsonResponse);
+      final basket = Basket.fromJson(jsonResponse as Map<String, dynamic>);
       return basket;
     } catch (e) {
       throw Exception('Failed to get basket: $e');
@@ -46,17 +46,17 @@ class BasketRepository extends Repository {
       {required String productId, int quantity = 1}) async {
     if (_basketId == null) await createBasket();
     try {
-      final response = await dio.post(
+      final response = await dio.post<dynamic>(
         '${config.host}/checkout/shopper-baskets/v1/organizations/${config.organizationId}/baskets/$_basketId/items?siteId=${config.siteId}',
         data: [
-          {'productId': productId, 'quantity': quantity}
+          {'productId': productId, 'quantity': quantity},
         ],
         options: Options(
           headers: {'Content-Type': 'application/json'},
         ),
       );
-      final dynamic jsonResponse = response.data;
-      Basket basket = Basket.fromJson(jsonResponse);
+      final jsonResponse = response.data;
+      final basket = Basket.fromJson(jsonResponse as Map<String, dynamic>);
       return basket;
     } catch (e) {
       throw Exception('Failed to add item to basket: $e');
@@ -64,8 +64,8 @@ class BasketRepository extends Repository {
   }
 
   Future<void> removeItemFromBasket(String productId) async {
-// TODO implement removeItemFromBasket (PATRI: No elimina un producto agregado anteriormente a la cesta)
-// TODO https://developer.salesforce.com/docs/commerce/commerce-api/references/shopper-baskets?meta=removeItemFromBasket
+    // TODO(Patri): implement removeItemFromBasket (PATRI: No elimina un producto agregado anteriormente a la cesta)
+    // TODO(Patri): https://developer.salesforce.com/docs/commerce/commerce-api/references/shopper-baskets?meta=removeItemFromBasket
 
     // if (_basketId == null) await createBasket();
     // try {
@@ -82,9 +82,11 @@ class BasketRepository extends Repository {
     // }
   }
 
-  Future<void> updateProductInBasket(
-      {required String productId, int quantity = 1}) async {
-    // TODO implement https://developer.salesforce.com/docs/commerce/commerce-api/references/shopper-baskets?meta=updateItemInBasket
+  Future<void> updateProductInBasket({
+    required String productId,
+    int quantity = 1,
+  }) async {
+    // TODO(Patri): implement https://developer.salesforce.com/docs/commerce/commerce-api/references/shopper-baskets?meta=updateItemInBasket
     // if (_basketId == null) await createBasket();
     // try {
     //   await dio.patch(
