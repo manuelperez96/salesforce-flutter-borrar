37edaba9dec366bfd59a9017bd0e8f6c30ad6ab7
Manuel Pérez Soto <manuelperezsoto@SELNK1350100586.corp.capgemini.com>
2024-08-27 12:56:25 +0200

    chore: clean warnings

--- lib/models/responses/category/category.dart
+++ lib/models/responses/category/category.dart
@@ -1,36 +1,36 @@
 class Category {
-  String id;
-  String name;
-  int onlineSubCategoriesCount;
-  String? pageDescription;
-  String? pageTitle;
-  String parentCategoryId;
-  String cHeaderMenuBanner;
-
-  Category({
+  const Category({
     required this.id,
     required this.name,
     required this.onlineSubCategoriesCount,
-    this.pageDescription,
-    this.pageTitle,
     required this.parentCategoryId,
     required this.cHeaderMenuBanner,
+    this.pageDescription,
+    this.pageTitle,
   });
 
   factory Category.fromJson(Map<String, dynamic> json) => Category(
-        id: json['id'],
-        name: json['name'],
-        onlineSubCategoriesCount: json['onlineSubCategoriesCount'],
-        pageDescription: json['pageDescription'],
-        pageTitle: json['pageTitle'],
-        parentCategoryId: json['parentCategoryId'],
+        id: json['id'] as String,
+        name: json['name'] as String,
+        onlineSubCategoriesCount: json['onlineSubCategoriesCount'] as int,
+        pageDescription: json['pageDescription'] as String,
+        pageTitle: json['pageTitle'] as String,
+        parentCategoryId: json['parentCategoryId'] as String,
         cHeaderMenuBanner: json['c_headerMenuBanner'] != null
-            ? parseBannerImage(json['c_headerMenuBanner'])
+            ? parseBannerImage(json['c_headerMenuBanner'] as String)
             : '',
       );
 
+  final String id;
+  final String name;
+  final int onlineSubCategoriesCount;
+  final String? pageDescription;
+  final String? pageTitle;
+  final String parentCategoryId;
+  final String cHeaderMenuBanner;
+
   static String parseBannerImage(String original) {
-    String imageUrl = original.substring(original.indexOf('src'));
+    var imageUrl = original.substring(original.indexOf('src'));
     imageUrl = imageUrl.substring(imageUrl.indexOf('"') + 1);
     imageUrl = imageUrl.substring(0, imageUrl.indexOf('"'));
     return imageUrl;
