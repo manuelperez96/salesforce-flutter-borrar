906c9e05ea371cef3dd5d77e09fd2c16206d878e
Manuel Pérez Soto <manuelperezsoto@LES-CND1526FMB.corp.capgemini.com>
2024-08-29 10:44:41 +0200

    refactor: exception thrown in ProductRepository

---
+++ lib/models/exception/product_exception.dart
@@ -0,0 +1,5 @@
+import 'package:sf_commerce_sdk/models/exception/base_api_exception.dart';
+
+class UnableToGetProductException extends BaseApiException {
+  const UnableToGetProductException([super.message]);
+}
